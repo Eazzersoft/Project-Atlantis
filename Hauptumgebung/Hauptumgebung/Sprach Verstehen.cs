a85556e35f3548d646e828865e32027cf3d1db2c
paul rex <paulalfrex@live.com>
2017-10-12 17:50:43 +0200

    a

---
+++ Hauptumgebung/Hauptumgebung/Sprach Verstehen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+
+namespace Hauptumgebung
+{
+    public class Sprach_Verstehen
+    {
+        
+
+
+
+
+
+
+
+
+    }
+}
